dc8321df12df1ec30a601334d85feaf1ee10e639
nonjene <reanfn89@gmail.com>
2017-08-28 19:01:42 +0800

    从项目读取配置

--- README.md
+++ README.md
@@ -1,13 +1,51 @@
 #ok-papa-cli
-A frontend projects solution.
+基于 webpack 的前端多页面解决方案。在此cli的帮助下快速部署前端页面：可多个输出或每个页面独立输出，并且包含多个环境切换、适合多个环境的最佳配置输出、其他路径代理等功能。
 
-#todo
-config 可自定义
 
-script
+#Feature
+* webpack使用1.x，支持ie8
 
+#Config
+papa.config.json：
+```json
+    {
+      static:{
+          ftp: {
+          host: "192.168.1.1",
+          port: "",
+          user: "user",
+          password: "ps"
+        },
+        remoteBasePath: "", //ftp的基目录，用于打印地址时方便排除。m.okpapa.com
+        remotePath: "/activity/{$target}",
+        localAssetPath: "build/activity/{$target}",
+        domainName: "http://m.okpapa.com",
+        cdnDomain: "https://images.okpapa.com"
+        proxyPort: 80,
+        servePort: 3005
+      }
+    }
+```
+
+
+#Todo
+万事开头难，然后中间难，最后结尾难
+1. 跑通webpack complie
+    1. 解耦npm命令
+1. cli: create a Project Solution
+1. write test
+1. 解耦resource的合并文件顺序
+1. 拆解各种配置：
+    1. webpack.config
+    1. 
+1. 
+
+
+
+
+<!-- script
 `
 "watch": "webpack --progress --colors --watch --config ./bin/webpack.config.js",
 "build": "webpack --progress --colors --config ./bin/webpack.config.js",
 "start": "serve -p 3005"
-`
+` -->
--- bin/config.js
+++ bin/config.js
@@ -2,120 +2,140 @@
  * Created by Nonjene on 2017/2/10.
  */
 
-const path = require("path");
-
-const StaticConfig = {
-    ftp: {
-        host: "192.168.1.1",
-        port: "",
-        user: "user",
-        password: "ps"
-    },
-    remoteBasePath: "", //ftp的基目录，用于打印地址时方便排除。m.okpapa.com
-    remotePath: "/activity/{$target}",
-    localAssetPath: "build/activity/{$target}",
-    domainName: "http://m.okpapa.com",
-    cdnDomain: "https://images.okpapa.com"
-};
-let config = Object.assign(
-    {
-        target: ["target_not_setted"],
-        env: "production",
-        fronendEnv: null,
-        proSpecific: null,
-        duan: ["pc", "m"],
-        proxyPort: 80,
-        servePort: 3005
-    },
-    StaticConfig
+const path = require('path');
+const fs = require('fs');
+const projConfPath = `${process.cwd()}/papa.config.json`;
+const projConf = Object.assign(
+  {
+    static: {}
+  },
+  (fs.existsSync(projConfPath) && JSON.parse(fs.readFileSync(projConfPath))) ||
+    {}
 );
 
-let EXPORT = "export";
-if (path.sep.indexOf("\\") > -1) {
-    EXPORT = "SET"; //windows
+const StaticConfig = Object.assign(
+  {
+    ftp: {
+      host: '192.168.1.1',
+      port: '',
+      user: 'user',
+      password: 'ps'
+    },
+    remoteBasePath: '', //ftp的基目录，用于打印地址时方便排除。m.okpapa.com
+    remotePath: '/activity/{$target}',
+    localAssetPath: 'build/activity/{$target}',
+    domainName: 'http://m.okpapa.com',
+    cdnDomain: 'https://images.okpapa.com',
+    proxyPort: 80,
+    servePort: 3005
+  },
+  projConf.static
+);
+
+// 之前写的有些代码用了 getConf() 来获取 StaticConfig，所以要合并进来
+let config = Object.assign(
+  {
+    target: ['target_not_setted'],
+    env: 'production',
+    fronendEnv: null,
+    proSpecific: null,
+    duan: ['pc', 'm']
+  },
+  StaticConfig
+);
+
+let EXPORT = 'export';
+if (path.sep.indexOf('\\') > -1) {
+  EXPORT = 'SET'; //windows
 }
 
 const getTargetCommander = (which = 0) => {
-    return `${EXPORT} BUILD_TARGET=` + config.target[which];
+  return `${EXPORT} BUILD_TARGET=` + config.target[which];
 };
 const getEnvCommander = () => {
-    let com = `${EXPORT} NODE_ENV=` + config.env;
-    //指定预发还是生产
-    if (config.proSpecific) {
-        com += `&&${EXPORT} PRO_SPECIFIC=${config.proSpecific}`;
-    }
-    return com;
+  let com = `${EXPORT} NODE_ENV=` + config.env;
+  //指定预发还是生产
+  if (config.proSpecific) {
+    com += `&&${EXPORT} PRO_SPECIFIC=${config.proSpecific}`;
+  }
+  return com;
 };
 const getDuanCommander = () => {
-    return config.duan ? `${EXPORT} DUAN=` + config.duan.join(",") : "";
+  return config.duan ? `${EXPORT} DUAN=` + config.duan.join(',') : '';
 };
 
 module.exports = Object.assign(
-    {
-        setTarget(name = "") {
-            //console.log(name)
-            config.target = name.split(",");
-        },
-        getTarget() {
-            return config.target;
-        },
-        setBuildAllScope(name = "") {
-            config.buildAllScope = name.split(",");
-        },
-        setEnv(env) {
-            config.env = env;
-        },
-        setDuan(duan) {
-            config.duan = duan.split(",");
-        },
-        getConf(prop) {
-            return prop ? config[prop] : config;
-        },
-        setConf(prop, val) {
-            config[prop] = val;
-        },
-        getEnvDesc() {
-            if (config.proSpecific) {
-                switch (config.proSpecific) {
-                    case "pre":
-                        return "预发环境😛";
-                    case "pro":
-                        return "生产环境😝";
-                    case "test":
-                        return "开发环境🤔";
-                    default:
-                        return "黑洞";
-                }
-            } else {
-                return "开发环境🤔";
-            }
-        },
-        getFrontendEnvDesc() {
-            switch (config.fronendEnv) {
-                case "test":
-                    return "测试环境🥝";
-                case "pre":
-                    return "预发环境🥑";
-                case "produce":
-                    return "生产环境🍆";
-                default:
-                    return "程序错误🙄";
-            }
-        },
-
-        combineBuild(which) {
-            return [getEnvCommander(), getTargetCommander(which), getDuanCommander(), "npm run build"].join(
-                "&&"
-            );
-        },
-        combineWatch(which) {
-            return [getEnvCommander(), getTargetCommander(which), getDuanCommander(), "npm run watch"].join(
-                "&&"
-            );
-        },
-        getTargetCommander,
-        getEnvCommander,
-        getDuanCommander
+  {
+    setTarget(name = '') {
+      //console.log(name)
+      config.target = name.split(',');
     },
-    StaticConfig
+    getTarget() {
+      return config.target;
+    },
+    setBuildAllScope(name = '') {
+      config.buildAllScope = name.split(',');
+    },
+    setEnv(env) {
+      config.env = env;
+    },
+    setDuan(duan) {
+      config.duan = duan.split(',');
+    },
+    getConf(prop) {
+      return prop ? config[prop] : config;
+    },
+    setConf(prop, val) {
+      config[prop] = val;
+    },
+    getEnvDesc() {
+      if (config.proSpecific) {
+        switch (config.proSpecific) {
+          case 'pre':
+            return '预发环境😛';
+          case 'pro':
+            return '生产环境😝';
+          case 'test':
+            return '开发环境🤔';
+          default:
+            return '黑洞';
+        }
+      } else {
+        return '开发环境🤔';
+      }
+    },
+    getFrontendEnvDesc() {
+      switch (config.fronendEnv) {
+        case 'test':
+          return '测试环境🥝';
+        case 'pre':
+          return '预发环境🥑';
+        case 'produce':
+          return '生产环境🍆';
+        default:
+          return '程序错误🙄';
+      }
+    },
+
+    combineBuild(which) {
+      return [
+        getEnvCommander(),
+        getTargetCommander(which),
+        getDuanCommander(),
+        'npm run build'
+      ].join('&&');
+    },
+    combineWatch(which) {
+      return [
+        getEnvCommander(),
+        getTargetCommander(which),
+        getDuanCommander(),
+        'npm run watch'
+      ].join('&&');
+    },
+    getTargetCommander,
+    getEnvCommander,
+    getDuanCommander
+  },
+  StaticConfig
 );
