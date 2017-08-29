d0df698a417e0e9317c77743848abadad68f7fde
nonjene <reanfn89@gmail.com>
2017-08-29 17:13:01 +0800

    format code

--- bin/frontend_conf.js
+++ bin/frontend_conf.js
@@ -8,11 +8,10 @@ const TPL = (props = {}) => `try{
     Object.freeze(window.publicConfig);
 }catch(e){}`;
 
-
-const fs = require("fs");
-const path = require("path");
-const { setConf } = require("./config");
-const {asyncEach} = require('./util/asyncEach');
+const fs = require('fs');
+const path = require('path');
+const { setConf } = require('./config');
+const { asyncEach } = require('./util/asyncEach');
 
 /**
  * 把所有config_v.js 修改。
@@ -30,43 +29,43 @@ const change = function(props, targets) {
 };
 
 const writeConf = function(props, target) {
-    return new Promise((resolve, reject) => {
-        const dir = `${process.cwd()}/src/${target}`;
-        if(!fs.existsSync(dir)) return reject(`指定的文件夹不存在：${target}`);
+  return new Promise((resolve, reject) => {
+    const dir = `${process.cwd()}/src/${target}`;
+    if (!fs.existsSync(dir)) return reject(`指定的文件夹不存在：${target}`);
 
-        fs.writeFile(path.join(dir, 'config_v.js'), TPL(props), err => {
-            if (err) return reject(err);
-            resolve();
-        });
+    fs.writeFile(path.join(dir, 'config_v.js'), TPL(props), err => {
+      if (err) return reject(err);
+      resolve();
     });
+  });
 };
 
 module.exports = {
-    promiseSetDone: {
-        then(cb) {
-            console.log("Haven't set frontend config, use last setting.");
-            return cb && cb("nothing");
-        }
-    },
-    setFrontEndConf(...arg) {
-        let [mode = "produce", debug = true, target] = arg;
-        if (arg.length === 2) {
-            target = debug;
-            debug = true;
-        }
-        if (arg.length === 1) {
-            throw new Error("构建错误：设置setFrontEndConf没有接收到target。");
-        }
-
-        if (mode === "produce") debug = false;
-        this.promiseSetDone = change(
-            {
-                mode,
-                debug
-            },
-            target
-        );
-        setConf("fronendEnv", mode);
-        return this.promiseSetDone;
+  promiseSetDone: {
+    then(cb) {
+      console.log("Haven't set frontend config, use last setting.");
+      return cb && cb('nothing');
     }
+  },
+  setFrontEndConf(...arg) {
+    let [mode = 'produce', debug = true, target] = arg;
+    if (arg.length === 2) {
+      target = debug;
+      debug = true;
+    }
+    if (arg.length === 1) {
+      throw new Error('构建错误：设置setFrontEndConf没有接收到target。');
+    }
+
+    if (mode === 'produce') debug = false;
+    this.promiseSetDone = change(
+      {
+        mode,
+        debug
+      },
+      target
+    );
+    setConf('fronendEnv', mode);
+    return this.promiseSetDone;
+  }
 };
