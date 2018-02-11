b0b4cd6874b216d594d807d94fe282aea148b262
nonjene <reanfn89@gmail.com>
2018-02-11 16:08:08 +0800

    add arg

--- bin/build.js
+++ bin/build.js
@@ -9,14 +9,14 @@ const chalk = require('chalk');
 const logInfo = require('./util/logInfo');
 const hasDuan = require('./util/hasDuan');
 
-const buildOne = function (which = 0) {
+const buildOne = function (which = 0, isNoLog = false) {
     //export NODE_ENV=production
     //export NODE_ENV=development
 
     // && export BUILD_TARGET=huodong1
     return new Promise((resolve, reject) => {
         const Tar = getConf('target')[which];
-        console.log(`${chalk.blue('正在编译活动：')}${Tar} 的 ${getConf('duan').join(',')} 端...`);
+        !isNoLog && console.log(`${chalk.blue('正在编译活动：')}${Tar} 的 ${getConf('duan').join(',')} 端...`);
 
         if(hasDuan(Tar).length<1){
             return reject(chalk.red(`没有找到：${Tar}，或里面没有m或pc文件夹，已略过，请检查拼写`) + '🤦')
@@ -24,7 +24,7 @@ const buildOne = function (which = 0) {
         exec(combineBuild(which), function (err, stdout, stderr) {
             if (err) reject(err);
 
-            console.log(stdout);
+            !isNoLog && console.log(stdout);
             resolve();
         });
 
@@ -32,8 +32,8 @@ const buildOne = function (which = 0) {
 
 };
 
-const build = function () {
-    logInfo();
+const build = function ({isNoLog}) {
+    !isNoLog && logInfo();
 
     return new Promise((resolve, reject) => {
         const List = getConf('target');
@@ -42,7 +42,7 @@ const build = function () {
             if (i > List.length - 1) {
                 return resolve();
             }
-            buildOne(i)
+            buildOne(i, isNoLog)
                 .then(() => run(i + 1))
                 .catch(reject);
 
