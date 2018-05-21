105b75fbf991b6acedbd86978c3e20b20c5a2154
nonjene <reanfn89@gmail.com>
2018-05-21 16:44:19 +0800

    del useless code

--- test/seed/src/proj1/pc/index.js
+++ test/seed/src/proj1/pc/index.js
@@ -4,18 +4,3 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 require("./index.scss");
-
-const path = document
-  .getElementById("triangles_logo")
-  .querySelectorAll("[fill-opacity]");
-
-const length = path.length;
-const show = i => {
-  requestAnimationFrame(() => {
-    path[i].setAttribute("fill-opacity", path[i].dataset.fillOpacity);
-    if (++i < length) {
-      return show(i);
-    }
-  });
-};
-show(0);
