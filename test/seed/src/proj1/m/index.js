13ea971ed180fccc1ef246ee77431b5cd6e2ed06
nonjene <reanfn89@gmail.com>
2018-05-23 17:07:19 +0800

    upd example

--- test/seed/src/proj1/m/index.js
+++ test/seed/src/proj1/m/index.js
@@ -10,9 +10,5 @@ const tools = require("modules/tools");
 
 //例子
 const hb = require("../mod.handlebars");
-const img1 = require('modules/img/example.jpg');
-document.getElementById('example2').innerHTML = hb({
-  img:{
-    img1
-  }
-})
+
+document.getElementById('example2').innerHTML = hb({})
